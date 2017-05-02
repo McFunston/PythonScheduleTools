e8794d6d069e99fbdaf0f94d9984211dc1b3cf7e
Mica Funston <MicaFunston@MAC26.local>
2017-05-02 12:12:57 -0400

    Add docstrings

--- FolderChecker.py
+++ FolderChecker.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # sample regex (^|\D)\d{6}($|\D)
-"""Given a path prints a list of all files contained"""
+"""Folder and file utilities"""
 from os import listdir
 from os.path import isfile, join
 
 def get_file_list(path):
+    """Given a path prints a list of all files contained
+    path -- Path that you want to get the file list from
+    Returns list of string (file names)"""
     try:
         file_list = [f for f in listdir(path) if isfile(join(path, f))]
         print(file_list)
-        return True
+        return file_list
     except:
         print("Failure")
         return False
-
-get_file_list("/Users/MicaFunston/Documents")
