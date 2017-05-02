d5ba5633b2c6aaa1f3fae2c743084d306a22915f
Mica Funston <MicaFunston@MAC26.local>
2017-05-02 12:33:25 -0400

    Add get_folder_list

--- FolderChecker.py
+++ FolderChecker.py
@@ -2,7 +2,7 @@
 # sample regex (^|\D)\d{6}($|\D)
 """Folder and file utilities"""
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 
 def get_file_list(path):
     """Given a path prints a list of all files contained
@@ -15,3 +15,17 @@ def get_file_list(path):
     except:
         print("Failure")
         return False
+
+def get_folder_list(path):
+    """Given a path prints a list of all files contained
+    path -- Path that you want to get the file list from
+    Returns list of string (file names)"""
+    try:
+        folder_list = [f for f in listdir(path) if isdir(join(path, f))]
+        print(folder_list)
+        return folder_list
+    except:
+        print("Failure")
+        return False
+
+f = get_folder_list("/Volumes/Dockets")
