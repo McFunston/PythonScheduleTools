422e10f057c9445235b4c697e222dae17104f58e
Mica Funston <mica.funston@gmail.com>
2017-05-06 17:36:36 -0400

    Add list_strip

--- ExcelReader.py
+++ ExcelReader.py
@@ -4,6 +4,14 @@ import sys
 import pprint
 from xlrd import open_workbook
 
+def list_strip(list_to_strip):
+    """Removes unecessary spaces from a list of cells
+    Args:
+       list_to_strip: A list of cells
+    Returns: A list of strings"""
+    stripped_list = [x.value.strip() for x in list_to_strip]
+    return stripped_list
+
 def get_all_jobs(file_name):
     with open_workbook(file_name) as book:
         sheet = book.sheet_by_index(0)
@@ -41,11 +49,11 @@ def get_jobs_by_id(file_name, id_column, job_id):
     Returns: A list containg the rows that match the job_id"""
     sheet = get_all_jobs(file_name)
     rows = sheet.get_rows()
-    jobs = [row for row in rows if row[id_column].value == job_id]
+    jobs = [list_strip(row) for row in rows if row[id_column].value == str(job_id)]
     return jobs
 
 #x = get_jobs('Printflow-ToDo.xls', [0, 3, 2, 7])
 #x = get_all_jobs('Printflow-ToDo.xls')
 #pprint.pprint(type(x.row(0)))
-job = get_jobs_by_id('Printflow-ToDo.xls', 3, "690114")
+job = get_jobs_by_id('Printflow-ToDo.xls', 3, 690114)
 pprint.pprint(job)
