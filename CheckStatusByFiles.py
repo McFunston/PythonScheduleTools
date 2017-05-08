4c779797b50e6e4dae26a46afb33822155d4ddf3
Mica Funston <MicaFunston@MAC26.local>
2017-05-08 10:29:45 -0400

    Add LMG specific stuff to CheckStatusByFiles

--- CheckStatusByFiles.py
+++ CheckStatusByFiles.py
@@ -4,14 +4,20 @@ by checking for the existance of files (if we have files why hasn't it been stat
 """
 import ExcelReader
 import FolderChecker
+import pprint
 
 def check_status_by_files(excel_filename, status_column, id_column, status, root_path, sub_path):
     jobs_to_check = ExcelReader.get_jobs_by_status(excel_filename, status_column, status)
     jobs_to_check = ExcelReader.get_just_ids(jobs_to_check, id_column)
     folders_to_check = FolderChecker.find_folders(root_path, jobs_to_check)
     folders_to_check = FolderChecker.folder_append(folders_to_check, sub_path)
-    candidates = FolderChecker.count_files(folders_to_check)
+    jobs = FolderChecker.count_files(folders_to_check)
+    candidates = [job for job in jobs if int(job[1]) > 0]
     return candidates
 
-TEST = check_status_by_files('Printflow-ToDo.xls', 2, 3, 'Files In', 'G:/TestWorkFolder/Dockets', '/Production/Print')
-print(TEST)
+#TEST = check_status_by_files('Printflow-ToDo.xls', 2, 3, 'Files In', 'G:/TestWorkFolder/Dockets', '/Production/Print')
+#print(TEST)
+TEST = check_status_by_files('Printflow-ToDo1.xls', 2, 3, 'Files In', '/Volumes/Dockets', '/Production/Print')
+TEST2 = check_status_by_files('Printflow-ToDo1.xls', 2, 3, 'Files In', '/Volumes/AraxiVolume_PRINERGYEPM_J/Jobs', '/System/SubPages')
+pprint.pprint(TEST)
+pprint.pprint(TEST2)
