b1da7ae14cfb0b861040dc99b40a1299a607aab7
Mica Funston <mica.funston@gmail.com>
2017-05-08 20:42:52 -0400

    Add doc string to find_in_csv

--- CSVReader.py
+++ CSVReader.py
@@ -3,10 +3,16 @@
 import csv
 import pprint
 def find_in_csv(file_name, search_string):
+    """Find a string within a CSV file
+    Args:
+        file_name: Name of csv file to search
+        search_string: String to be searched for
+    Returns: A list of dates and print jobs
+    """
     with open(file_name, newline='') as csvfile:
         reader = csv.reader(x.replace('\0', '') for x in csvfile)
         #log_file = csv.reader(csvfile, delimiter=',', quotechar='"')
-        findings = [[r[0],r[1]] for r in reader if len(r) > 1 and search_string in r[1] ]
+        findings = [[r[0], r[1]] for r in reader if len(r) > 1 and search_string in r[1]]
         return findings
 FOUND = find_in_csv('Job Log.csv', '690137')
 pprint.pprint(FOUND)
