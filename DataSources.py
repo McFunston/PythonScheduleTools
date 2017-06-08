a033acb5b36423028f90ab9f7d103491768ee9f3
Mica Funston <mica.funston@gmail.com>
2017-06-08 19:10:25 -0400

    Add source_name to DataSources

--- DataSources.py
+++ DataSources.py
@@ -1,27 +1,27 @@
 from abc import ABC, abstractmethod
 
 class ExcelStatus:
-    def __init__(self, path, id_column, status_column):
+    def __init__(self, source_name, path, id_column, status_column):
         self.path = path
         self.id_column = id_column
         self.status_column = status_column
 
 class LogFileStatus:
-    def __init__(self, path, status):
+    def __init__(self, source_name, path, status):
         self.path = path
         self.status = status
 
 class FileSystemStatus:
-    def __init__(self, path, sub_path, status):
+    def __init__(self, source_name, path, sub_path, status):
         self.path = path
         self.sub_path = sub_path
         self.status = status
 
 class FileStatus(FileSystemStatus):
-    def __init__(self, path, sub_path, status):
+    def __init__(self, source_name, path, sub_path, status):
         super().__init__(self, path, sub_path, status)
 
 class FolderStatus(FileSystemStatus):
-    def __init__(self, path, sub_path, status):
+    def __init__(self, source_name, path, sub_path, status):
         super().__init__(self, path, sub_path, status)
     
