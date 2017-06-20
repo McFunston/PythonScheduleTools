feac7d2212d6da66bd20f491eafebf9560d83bd9
Mica Funston <MicaFunston@MAC26.local>
2017-06-20 10:51:27 -0400

    Create separate PDF and Xerox log

--- Logger.sh
+++ Logger.sh
@@ -1,3 +1,5 @@
 set -ex
 /usr/local/bin/Python3 /Users/MicaFunston/Projects/PythonScheduleTools/FolderWatcher.py "/Volumes/share/" "/Users/MicaFunston/Projects/PythonScheduleTools/ProofLog.csv"
 /usr/local/bin/Python3 /Users/MicaFunston/Projects/PythonScheduleTools/FolderWatcher.py "/Volumes/PTDATA/PDFs" "/Users/MicaFunston/Projects/PythonScheduleTools/ProofLog.csv"
+/usr/local/bin/Python3 /Users/MicaFunston/Projects/PythonScheduleTools/FolderWatcher.py "/Volumes/share/" "/Users/MicaFunston/Projects/PythonScheduleTools/Xerox-ProofLog.csv"
+/usr/local/bin/Python3 /Users/MicaFunston/Projects/PythonScheduleTools/FolderWatcher.py "/Volumes/PTDATA/PDFs" "/Users/MicaFunston/Projects/PythonScheduleTools/PDF-ProofLog.csv"
