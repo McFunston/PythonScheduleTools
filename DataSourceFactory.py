0eadb036b64e895fcd9effca86f078aaef62dc98
Mica Funston <mica.funston@gmail.com>
2017-07-08 18:45:07 -0400

    Make sure log check_status is working

--- DataSourceFactory.py
+++ DataSourceFactory.py
@@ -2,7 +2,7 @@ import json
 import pprint
 import DataSources
 
-DSFFILE = 'DataSources.json'
+DSFFILE = 'TestData/TestDataSources.json'
 
 class DataSourceFactory:
 
@@ -30,14 +30,16 @@ class DataSourceFactory:
                 if 'File Name' in value['Data Type']:
                     source = DataSources.FileStatusByName(value)
                 job_sources[key] = source
-            print(id_number)
+            #print(id_number)
             all_sources[id_number] = job_sources
 
         return all_sources
 
 dsf = DataSourceFactory()
-id_list = ['687203', '689398']
+id_list = ['687203', '689398', '690714', '684396']
 sources = dsf.get_sources(id_list)
-print(sources['687203']['P drive Files - Huntclub'].check_status('Files In','687203'))
+print(sources['687203']['P drive Files - Huntclub'].check_status('Files In', '685543'))
+print(sources['689398']['P drive Files - Huntclub'].check_status('Files In', '689398'))
 
-
+print(sources['690714']['PDF Proof Out - Huntclub'].check_status('PDF Proof Out', '690714'))
+print(sources['684396']['Xerox Proof Out - Huntclub'].check_status('Proof Out', '684396'))
