a20657eb8ab25033731758adae46a41a2d357eec
Mica Funston <MicaFunston@MAC26.local>
2017-07-25 14:22:08 -0400

    Remove unecessary methods from FolderChecker

--- Services/FolderCheckerClass.py
+++ Services/FolderCheckerClass.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # sample regex (^|\D)\d{6}($|\D)
 """Folder and file utilities"""
-import re
+
 import os
 import os.path
 from os import listdir
@@ -11,42 +11,27 @@ import time
 
 class FolderChecker():
 
-    def get_file_list(self, path):
-        """Given a path returns a list of all files contained,
-        Args: path: Path that you want to get the file list from
-        Returns: list of string (file names)"""
-        try:
-            file_list = [f for f in listdir(path) if isfile(join(path, f))]
-            return file_list
-        except FileNotFoundError:
-            print("Failure in get_file_list")
-            return ["Bad folder structure"]
+    def __init__(self, path):
+        self.path = path
 
-
-    def get_full_file_list(self, path):
+    def _get_full_file_list(self):
         """Given a path returns a list of all files contained,
         with full path,
         Args: path: Path that you want to get the file list from
         Returns: list of string (file names)"""
-        file_list = list()
-        for dirpath, dirnames, filenames in os.walk(path):
-            for filename in [f for f in filenames if f.endswith(".pdf")]:
-                if filename[0] != ".":
-                    file_list.append(os.path.join(dirpath, filename))
-                #print(os.path.join(dirpath, filename))
+        file_list = [join(self.path, f) for f in listdir(
+            self.path) if isfile(join(self.path, f)) and f[0] != "."]
         return file_list
 
-
-    def get_file_list_with_date(self, path):
+    def get_file_list(self):
         """Given a path, returns a list of dates and file paths
         Args: path: Path that you want to get the file list from
         Returns: list of [date, path]
         """
-        file_list = self.get_full_file_list(path)
+        file_list = self._get_full_file_list()
         file_list_with_date = list()
         for file in file_list:
             if file[0] != ".":
-                #file = os.path.normpath(file)
                 try:
                     file_with_date = [time.ctime(
                         os.path.getmtime(file)), os.path.basename(file)]
@@ -55,50 +40,38 @@ class FolderChecker():
                     print(file + " is not a real file")
         return file_list_with_date
 
-
-    def get_folder_list(self, path):
-        """Given a path returns a list of all folders contained,
-        path -- Path that you want to get the file list from
-        Returns list of string (folder names)"""
-        try:
-            folder_list = [f for f in listdir(path) if isdir(join(path, f))]
-            return folder_list
-        except FileNotFoundError:
-            print("Failure in get_folder_list" + path)
-            return
-
-    def get_folder_list_with_date(self, path):
+    def get_folder_list(self):
         """Given a path, returns a list of dates and folders
         Args: path: Path that you want to get the file list from
         Returns: list of [date, foldernames]
         """
         folder_list_with_date = list()
-        folder_list = [f for f in listdir(path) if isdir(join(path, f))]
+        folder_list = [f for f in listdir(self.path) if isdir(join(self.path, f))]
         for folder in folder_list:
-            folder_with_date = [time.ctime(os.path.getmtime(path+'/'+folder)), folder]
+            folder_with_date = [time.ctime(
+                os.path.getmtime(self.path + '/' + folder)), folder]
             folder_list_with_date.append(folder_with_date)
         return folder_list_with_date
 
-    def find_folders(self, path, names):
+    def _find_folders(self, names):
         """Returns a list of the complete paths of folders if they exists within the given path.
         path -- List of paths to search
         names -- List of partial or complete names to search for
         Returns a list of paths as strings.
         """
         found_folders = list()
-        folder_candidates = self.get_folder_list(path)
+        folder_candidates = self.get_folder_list()
         if folder_candidates:
             for name in names:
                 for folder in folder_candidates:
                     if name in folder:
-                        fullpath = path + '/' + folder
+                        fullpath = self.path + '/' + folder
                         found_folders.append(fullpath)
             return found_folders
         else:
             return
 
-
-    def folder_append(self, folders, sub_folder):
+    def _folder_append(self, folders, sub_folder):
         """Appends subfolder paths onto a list of folders
             Args:
                 folders: list of folders to append to
@@ -114,41 +87,7 @@ class FolderChecker():
         else:
             return
 
-
-    def count_files(self, folders):
-        """Count the files contained in a folder
-        Args:
-            folder: Path to check
-        Returns: [Folder Path, Count of files contained (int), List of files]
-        """
-        folder_size = list()
-        if folders:
-            for folder in folders:
-                #file_list = get_file_list(folder)
-                file_list = self.get_file_list_with_date(folder)
-                if file_list:
-                    folder_size.append([folder, len(file_list), file_list])
-            return folder_size
-        else:
-            return
-
-
-    def get_files_containing(self, path, search_string):
-        """Gets a list of files in a path that contain a string
-        Args:
-            path: Path to check
-            search_string: string to search for
-        Returns: [data, file including full path]
-        """
-        files = self.get_file_list_with_date(path)
-        file_list = list()
-        for file in files:
-            if search_string in file[1]:
-                file_list.append(file)
-        return file_list
-
-
-    def folder_exists(self, folder):
+    def _folder_exists(self, folder):
         """Check it a folder exists at a given path
         Args:
             folder: Path of folder to check
@@ -159,7 +98,5 @@ class FolderChecker():
         else:
             return False
 
-
-#TEST = get_folder_list_with_date("TestData/Dockets/672143/Production")
-#print(TEST)
-
+# fc = FolderChecker('TestData')
+# print(fc.get_folder_list())
