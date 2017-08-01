a0137a884498bd31af13ccb747ed31ea03db1a90
Mica Funston <MicaFunston@MAC26.local>
2017-08-01 13:35:07 -0400

    Add find_job to ListShaperClass

--- ListShaper.py
+++ ListShaper.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 
 def find_earlier(item1, item2):
-    """Given two jobs [date, string] returns the earlier one"""
     item1_date_time = datetime.strptime(item1[0], '%a %b %d %H:%M:%S %Y')
     item2_date_time = datetime.strptime(item2[0], '%a %b %d %H:%M:%S %Y')
     if item1_date_time < item2_date_time:
@@ -12,17 +11,6 @@ def find_earlier(item1, item2):
     else:
         return item2
 
-def space_strip(list_to_strip):
-    """Removes unecessary spaces from a list of [date, string]
-    Args:
-    list_to_strip: A list of cells
-    Returns: A list of strings"""
-    stripped_list = list()
-    for item_to_strip in list_to_strip:
-        item_to_strip[1] = item_to_strip[1].strip()
-        stripped_list.append(item_to_strip)
-    #stripped_list = [x.strip() for x in list_to_strip]
-    return stripped_list
 
 def job_lister(results):
     shaped_results = list()
@@ -41,24 +29,24 @@ def job_lister(results):
 
 
 def job_counter(jobs_list, job_number):
-    """Returns the number of times that job_number occurs in a list of [date, string]"""
-    count = 0
+    formed_job = list()
+    formed_job.append(job_number)
+    formed_job.append(0)
     for job in jobs_list:
         if job_number in job[1]:
-            count += 1            
-    return count
+            formed_job[1] += 1
+            formed_job.append([job])
+    return formed_job
 
 
 class MyTest(unittest.TestCase):
     item1 = ['Tue May 30 09:15:42 2017', "item1r684500"]
     item2 = ['Wed May 24 15:44:57 2017', "700000item2"]
     item3 = ['Wed May 24 15:44:57 2017', "item3r684500item2"]
-    item4 = ['Tue May 30 09:15:42 2017', "item1r685300 "]
     items = list()
     items.append(item1)
     items.append(item2)
     items.append(item3)
-    items.append(item4)
 
     def test_find_earlier(self):
         # Arrange
@@ -71,7 +59,7 @@ class MyTest(unittest.TestCase):
 
     def test_job_lister(self):
         # Arrange
-        expected = ["684500", '685300', "700000"]
+        expected = ["684500", "700000"]
         # Act
         actual = job_lister(self.items)
         # Assert
@@ -79,18 +67,10 @@ class MyTest(unittest.TestCase):
 
     def test_job_counter(self):
         # Arrange
-        expected = 2
+        expected = ["684500", 2, [['Tue May 30 09:15:42 2017', 'item1r684500']], [
+            ['Wed May 24 15:44:57 2017', 'item3r684500item2']]]
         # Act
         actual = job_counter(self.items, "684500")
-        # Assert
-        self.assertEqual(actual, expected)
-
-    def test_space_strip(self):
-        # Arrange
-        expected = ['Tue May 30 09:15:42 2017', "item1r685300"]
-        # Act
-        actual = space_strip(self.items)
-        actual = actual[3]
         # Assert
         self.assertEqual(actual, expected)
 
--- Services/ListShaperClass.py
+++ Services/ListShaperClass.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 def find_earlier(item1, item2):
+    """Given two jobs [date, string] returns the earlier one"""
     item1_date_time = datetime.strptime(item1[0], '%a %b %d %H:%M:%S %Y')
     item2_date_time = datetime.strptime(item2[0], '%a %b %d %H:%M:%S %Y')
     if item1_date_time < item2_date_time:
@@ -11,6 +12,17 @@ def find_earlier(item1, item2):
     else:
         return item2
 
+def space_strip(list_to_strip):
+    """Removes unecessary spaces from a list of [date, string]
+    Args:
+    list_to_strip: A list of cells
+    Returns: A list of strings"""
+    stripped_list = list()
+    for item_to_strip in list_to_strip:
+        item_to_strip[1] = item_to_strip[1].strip()
+        stripped_list.append(item_to_strip)
+    #stripped_list = [x.strip() for x in list_to_strip]
+    return stripped_list
 
 def job_lister(results):
     shaped_results = list()
@@ -27,26 +39,33 @@ def job_lister(results):
         found_jobs.sort()
     return found_jobs
 
+def find_job(jobs_list, search_string):
+    """Searches for search_string within a jobs list [Date, String]. Returns a list of [Date, String]"""
+    found_jobs = list()
+    for job in jobs_list:
+        if search_string in job[1]:
+            found_jobs.append(job)
+    return found_jobs
 
 def job_counter(jobs_list, job_number):
-    formed_job = list()
-    formed_job.append(job_number)
-    formed_job.append(0)
+    """Returns the number of times that job_number occurs in a list of [date, string]"""
+    count = 0
     for job in jobs_list:
         if job_number in job[1]:
-            formed_job[1] += 1
-            formed_job.append([job])
-    return formed_job
+            count += 1            
+    return count
 
 
 class MyTest(unittest.TestCase):
     item1 = ['Tue May 30 09:15:42 2017', "item1r684500"]
     item2 = ['Wed May 24 15:44:57 2017', "700000item2"]
     item3 = ['Wed May 24 15:44:57 2017', "item3r684500item2"]
+    item4 = ['Tue May 30 09:15:42 2017', "item1r685300 "]
     items = list()
     items.append(item1)
     items.append(item2)
     items.append(item3)
+    items.append(item4)
 
     def test_find_earlier(self):
         # Arrange
@@ -59,7 +78,7 @@ class MyTest(unittest.TestCase):
 
     def test_job_lister(self):
         # Arrange
-        expected = ["684500", "700000"]
+        expected = ["684500", '685300', "700000"]
         # Act
         actual = job_lister(self.items)
         # Assert
@@ -67,10 +86,26 @@ class MyTest(unittest.TestCase):
 
     def test_job_counter(self):
         # Arrange
-        expected = ["684500", 2, [['Tue May 30 09:15:42 2017', 'item1r684500']], [
-            ['Wed May 24 15:44:57 2017', 'item3r684500item2']]]
+        expected = 2
         # Act
         actual = job_counter(self.items, "684500")
+        # Assert
+        self.assertEqual(actual, expected)
+
+    def test_space_strip(self):
+        # Arrange
+        expected = ['Tue May 30 09:15:42 2017', "item1r685300"]
+        # Act
+        actual = space_strip(self.items)
+        actual = actual[3]
+        # Assert
+        self.assertEqual(actual, expected)
+    
+    def test_find_job(self):
+        # Arrange
+        expected = [['Tue May 30 09:15:42 2017', "item1r685300 "]]
+        # Act
+        actual = find_job(self.items, '685300')
         # Assert
         self.assertEqual(actual, expected)
 
