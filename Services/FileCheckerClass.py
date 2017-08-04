06c1d7ed0a7076dba3014fcf025468722966ef7a
Mica Funston <mica.funston@gmail.com>
2017-08-04 19:37:17 -0400

    Clean up FolderCheckerClass

---
+++ Services/FileCheckerClass.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# sample regex (^|\D)\d{6}($|\D)
+"""Class for checking files and modification dates"""
+
+import os
+import os.path
+from os import listdir
+from os.path import isfile, isdir, join
+import time
+import Cache
+
+
+class FileChecker():
+
+    def __init__(self, path):
+        self.path = path
+
+    def _get_full_file_list(self):
+        """Given a path returns a list of all files contained,
+        with full path,
+        Args: path: Path that you want to get the file list from
+        Returns: list of string (file names)"""
+        file_list = [join(self.path, f) for f in listdir(
+            self.path) if isfile(join(self.path, f)) and f[0] != "."]
+        return file_list
+
+    def _get_uncached_data(self):
+        """Given a path, returns a list of dates and file paths
+        Args: path: Path that you want to get the file list from
+        Returns: list of [date, path]
+        """
+        file_list = self._get_full_file_list()
+        file_list_with_date = list()
+        for file in file_list:
+            if file[0] != ".":
+                try:
+                    file_with_date = [time.ctime(
+                        os.path.getmtime(file)), os.path.basename(file)]
+                    file_list_with_date.append(file_with_date)
+                except FileNotFoundError:
+                    print(file + " is not a real file")
+        return file_list_with_date
+
+    def _get_data(self):
+        cache = Cache.Cache()
+        return cache.get_list(self.path, self._get_uncached_data)
+
+    def get_list(self):
+        return self._get_data()
+
+fc = FileChecker('TestData')
+print(fc.get_list())
+print(fc.get_list())
--- Services/FolderCheckerClass.py
+++ Services/FolderCheckerClass.py
@@ -7,6 +7,7 @@ import os.path
 from os import listdir
 from os.path import isfile, isdir, join
 import time
+import Cache
 
 
 class FolderChecker():
@@ -14,44 +15,23 @@ class FolderChecker():
     def __init__(self, path):
         self.path = path
 
-    def _get_full_file_list(self):
-        """Given a path returns a list of all files contained,
-        with full path,
-        Args: path: Path that you want to get the file list from
-        Returns: list of string (file names)"""
-        file_list = [join(self.path, f) for f in listdir(
-            self.path) if isfile(join(self.path, f)) and f[0] != "."]
-        return file_list
-
-    def get_file_list(self):
-        """Given a path, returns a list of dates and file paths
-        Args: path: Path that you want to get the file list from
-        Returns: list of [date, path]
-        """
-        file_list = self._get_full_file_list()
-        file_list_with_date = list()
-        for file in file_list:
-            if file[0] != ".":
-                try:
-                    file_with_date = [time.ctime(
-                        os.path.getmtime(file)), os.path.basename(file)]
-                    file_list_with_date.append(file_with_date)
-                except FileNotFoundError:
-                    print(file + " is not a real file")
-        return file_list_with_date
-
-    def get_folder_list(self):
+    def _get_uncached_data(self):
         """Given a path, returns a list of dates and folders
         Args: path: Path that you want to get the file list from
         Returns: list of [date, foldernames]
         """
         folder_list_with_date = list()
-        folder_list = [f for f in listdir(self.path) if isdir(join(self.path, f))]
+        folder_list = [f for f in listdir(
+            self.path) if isdir(join(self.path, f))]
         for folder in folder_list:
             folder_with_date = [time.ctime(
                 os.path.getmtime(self.path + '/' + folder)), folder]
             folder_list_with_date.append(folder_with_date)
         return folder_list_with_date
+
+    def _get_data(self):
+        cache = Cache.Cache()
+        return cache.get_list(self.path, self._get_uncached_data)
 
     def _find_folders(self, names):
         """Returns a list of the complete paths of folders if they exists within the given path.
@@ -98,5 +78,9 @@ class FolderChecker():
         else:
             return False
 
-# fc = FolderChecker('TestData')
-# print(fc.get_folder_list())
+    def get_list(self):
+        return self._get_data()
+
+fc = FolderChecker('TestData')
+print(fc.get_list())
+print(fc.get_list())
