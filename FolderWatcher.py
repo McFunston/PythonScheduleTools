38cc5e60740820091f786d94ea1ce6178cf04b32
Mica Funston <MicaFunston@MAC26.local>
2018-02-02 16:24:12 -0500

    Make Folder Watcher prune stuff over 2 months old.

--- FolderWatcher.py
+++ FolderWatcher.py
@@ -4,7 +4,11 @@ import sys
 import csv
 import CSVReader
 import FolderChecker
+from datetime import datetime
+from datetime import timedelta
 
+def datetime_parser(dt):
+    return datetime.strptime(dt, '%a %b %d %H:%M:%S %Y')
 
 def folder_watcher(folder, logfile):
     files = FolderChecker.get_file_list_with_date(folder)
@@ -17,11 +21,13 @@ def folder_watcher(folder, logfile):
         file.close()
     current_log = CSVReader.read_log(logfile)
     log = list()
-    log = [[l[0], l[1]] for l in current_log]
-    files_to_add = list()
-    for file in files:
-        if file not in log:
+    log = [[l[0], l[1]] for l in current_log if datetime_parser(l[0]) > datetime.today() - timedelta(days=60)]
+    
+    for file in files:        
+        if file not in log and datetime_parser(file[0]) > datetime.today() - timedelta(days=60):
+            print("Found one")
             log.append(file)
+        
     # log.append(files_to_add)
     with open(logfile, 'w', newline='', encoding='latin-1') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
@@ -29,6 +35,7 @@ def folder_watcher(folder, logfile):
             writer.writerow(file_to_add)
 # quotechar='|'
 
+#folder_watcher("/Volumes/PTDATA/PDFs", "/Users/MicaFunston/Projects/PythonScheduleTools/ProofLog-test.csv")
 
 #folder_watcher("//192.168.113.50/share/", "ProofLog2.csv")
 if __name__ == "__main__":
