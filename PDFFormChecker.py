f38223c32dab5d3541776e0b4494b3aa460cfde5
Mica Funston <mica.funston@gmail.com>
2018-02-11 12:23:31 -0500

    Add PDFFormChecker.py

---
+++ PDFFormChecker.py
@@ -0,0 +1,22 @@
+import sys
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdftypes import resolve1
+
+
+def PDFFormChecker(path, fieldName, trueValue):
+    #filename = sys.argv[1]
+    if 'approved' in path.lower():
+        return True
+    fp = open(path, 'rb')
+    parser = PDFParser(fp)
+    doc = PDFDocument(parser)
+    fields = resolve1(doc.catalog['AcroForm'])['Fields']
+    for i in fields:
+        field = resolve1(i)
+        name, value = field.get('T'), field.get('V')
+        if fieldName in str(name) and trueValue in str(value):
+            return True
+    return False
+
+print(PDFFormChecker('G:/OneDrive/P001R10-Ott3_01.pdf', 'PROOF OK AS SUBMITTED', 'Yes'))
