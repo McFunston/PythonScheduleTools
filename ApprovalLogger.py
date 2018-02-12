4ddbbac80b8164545c02c4064c2c05f381b9b402
Mica Funston <mica.funston@gmail.com>
2018-02-11 19:48:55 -0500

    Make ApprovalLogger not check already approved jobs

--- ApprovalLogger.py
+++ ApprovalLogger.py
@@ -30,19 +30,20 @@ def logger(path, csv_path, sub_path):
     current_log = CSVReader.read_log(csv_path)
     log = list()
     log = [[l[0], l[1]] for l in current_log if datetime_parser(l[0]) > datetime.today() - timedelta(days=60)]
-
+    logged_jobs = [l[1] for l in log]
     for job in job_list:
-        full_path = get_full_path(path, job, sub_path)
-        forms = FolderChecker.get_file_list_with_date(full_path)
-        log_entry = list()
-        for form in forms:
-            log_entry.append(form[0])
-            log_entry.append(job)
-            potential_log_addition = list()
-            potential_log_addition.append(log_entry)
-            if potential_log_addition[0] not in log and datetime_parser(form[0]) < datetime.today() - timedelta(days=7): 
-                    if PDFFormChecker.PDFFormChecker(full_path + "/" + form[1], 'PROOF OK AS SUBMITTED', 'Yes'):
-                        log.append(log_entry)
+        if str(job) not in logged_jobs:
+            full_path = get_full_path(path, job, sub_path)
+            forms = FolderChecker.get_file_list_with_date(full_path)
+            log_entry = list()
+            for form in forms:
+                log_entry.append(form[0])
+                log_entry.append(job)
+                potential_log_addition = list()
+                potential_log_addition.append(log_entry)
+                if datetime_parser(form[0]) > datetime.today() - timedelta(days=14): 
+                        if PDFFormChecker.PDFFormChecker(full_path + "/" + form[1], 'PROOF OK AS SUBMITTED', 'Yes'):
+                            log.append(log_entry)
     with open(csv_path, 'w', newline='', encoding='latin-1') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         for file_to_add in log:
